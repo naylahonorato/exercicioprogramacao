5a1d473ec632e8b2d3536fcd447abcdfbc2734af
nayla honorato <naylahonorato@hotmail.com>
2021-06-15 17:21:36 -0300

    Exercicios ciclo

---
+++ exerciciociclo/ex1.js
@@ -0,0 +1,4 @@
+// Exercício 01
+for (let i=1; i<=5; i++){
+    console.log("Olá mundo.");
+}
---
+++ exerciciociclo/ex3.js
@@ -0,0 +1,6 @@
+// Exercício 3
+for (let i=1; i<=10; i++){
+    for (let a=0; a<=10; a++){
+        console.log(i*a);
+    }
+}
